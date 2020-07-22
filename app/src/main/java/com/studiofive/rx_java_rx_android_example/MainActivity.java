1ddf3c02187efd7972149d68b62dc33f4d32c64f
Samuel-dot-cloud <successam336@gmail.com>
2020-07-22 19:59:12 +0300

    Using a buffer operator to track UI interaction

--- app/src/main/java/com/studiofive/rx_java_rx_android_example/MainActivity.java
+++ app/src/main/java/com/studiofive/rx_java_rx_android_example/MainActivity.java
@@ -7,7 +7,10 @@ import android.os.Handler;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.jakewharton.rxbinding4.view.RxView;
+
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.annotations.NonNull;
@@ -21,6 +24,7 @@ import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.functions.Function;
 import io.reactivex.rxjava3.functions.Predicate;
 import io.reactivex.rxjava3.schedulers.Schedulers;
+import kotlin.Unit;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -35,34 +39,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         text = findViewById(R.id.text);
 
-// Create an Observable
-        Observable<Task> taskObservable = Observable
-                .fromIterable(DataSource.createTasksList())
-                .subscribeOn(Schedulers.io());
-
-        taskObservable
-                .buffer(2) // Apply the Buffer() operator
+        // detect clicks to a button
+        RxView.clicks(findViewById(R.id.button))
+                .map(new Function<Unit, Integer>() { // convert the detected clicks to an integer
+                    @Override
+                    public Integer apply(Unit unit) throws Exception {
+                        return 1;
+                    }
+                })
+                .buffer(4, TimeUnit.SECONDS) // capture all the clicks during a 4 second interval
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<List<Task>>() { // Subscribe and view the emitted results
+                .subscribe(new Observer<List<Integer>>() {
                     @Override
                     public void onSubscribe(Disposable d) {
+                        disposable.add(d); // add to disposables to you can clear in onDestroy
                     }
                     @Override
-                    public void onNext(List<Task> tasks) {
-                        Log.d(TAG, "onNext: bundle results: -------------------");
-                        for(Task task: tasks){
-                            Log.d(TAG, "onNext: " + task.getDescription());
-                        }
+                    public void onNext(List<Integer> integers) {
+                        Log.d(TAG, "onNext: You clicked " + integers.size() + " times in 4 seconds!");
                     }
-
                     @Override
                     public void onError(Throwable e) {
+
                     }
                     @Override
                     public void onComplete() {
+
                     }
                 });
-
 
     }
 
